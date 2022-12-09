121fff5526cdf17d7579b707b55decfd487929e5
ZdenekPetrik <zdenekpetrik@seznam.cz>
2022-12-09 10:55:58 +0100

    VS 2022

--- EvitelApp2/Controls/ctrlEventLog.designer.cs
+++ EvitelApp2/Controls/ctrlEventLog.designer.cs
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.dgw = new EvitelApp2.MyUserControl.ucDataGridView();
+            this.dgw = new EvitelApp2.MyUserControl.ucDataGridView(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dgw)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,7 +43,7 @@
             this.dgw.AllowUserToResizeRows = false;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.Green;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.GrayText;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
@@ -52,26 +52,28 @@
             this.dgw.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dgw.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dgw.Location = new System.Drawing.Point(28, 13);
+            this.dgw.Location = new System.Drawing.Point(33, 15);
+            this.dgw.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.dgw.Name = "dgw";
-            this.dgw.Size = new System.Drawing.Size(197, 105);
+            this.dgw.Size = new System.Drawing.Size(230, 121);
             this.dgw.TabIndex = 1;
             this.dgw.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgw_CellContentClick);
             this.dgw.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgw_ColumnHeaderMouseClick_1);
             // 
             // ctrlEventLog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.dgw);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "ctrlEventLog";
-            this.Size = new System.Drawing.Size(972, 414);
+            this.Size = new System.Drawing.Size(1134, 478);
             this.Load += new System.EventHandler(this.ctrlEventLog_Load);
             this.Resize += new System.EventHandler(this.ctrlEventLog_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.dgw)).EndInit();
---
+++ EvitelApp2/Controls/ucIntervents.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace EvitelApp2.Controls
+{
+    partial class ucIntervents
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ucDataGridView1 = new EvitelApp2.MyUserControl.ucDataGridView(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ucDataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ucDataGridView1
+            // 
+            this.ucDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ucDataGridView1.Location = new System.Drawing.Point(73, 28);
+            this.ucDataGridView1.Name = "ucDataGridView1";
+            this.ucDataGridView1.RowTemplate.Height = 25;
+            this.ucDataGridView1.Size = new System.Drawing.Size(519, 225);
+            this.ucDataGridView1.TabIndex = 0;
+            // 
+            // ucIntervents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ucDataGridView1);
+            this.Name = "ucIntervents";
+            this.Size = new System.Drawing.Size(1027, 443);
+            ((System.ComponentModel.ISupportInitialize)(this.ucDataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyUserControl.ucDataGridView ucDataGridView1;
+    }
+}
